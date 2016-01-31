c6191e46972ab2cf512d1fc825d60fa225bad618
David Basalla <davidbasalla@gmail.com>
2016-01-31 12:34:44 +0000

    Add lookups for the different types

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,9 +1,9 @@
 class SalesController < ApplicationController
-  def index
-    @postcode = postcode
+  helper_method :postcode
 
+  def index
     if time_format_monthly?
-      @sales_by_time_group = Sale.in_postcode(@postcode).flats.order(:date).group_by { |s| s.date.beginning_of_month }
+      @sales_by_time_group = sales.group_by { |s| s.date.beginning_of_month }
 
       @sales = @sales_by_time_group.map do | start_of_month, sales_of_month |
         sum = sales_of_month.sum { |s| s.amount }
@@ -25,8 +25,21 @@ class SalesController < ApplicationController
     end
   end
 
+  def sales
+    Sale.property_types(property_type_params).in_postcode(postcode).order(:date)
+  end
+
   def postcode
     params[:search_postcode] || "N1"
+  end
+
+  def property_type_params
+    types = []
+    types << "flats" if params["toggle_flats"]
+    types << "detached" if params["toggle_detached"]
+    types << "semi_detached" if params["toggle_semi_detached"]
+    types << "terraced" if params["toggle_terraced"]
+    types
   end
 
   def time_format_monthly?
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -10,4 +10,31 @@ class Sale < ActiveRecord::Base
   def self.average_amount(sales)
     sales.map { |s| s.amount }
   end
+
+  def self.property_types(params)
+    Sale.where(property_types_where_values(params).join(" OR "))
+  end
+
+  def self.property_types_where_values(params)
+    Sale.send_chained_where_values(params).where_values
+  end
+
+  def self.send_chained_where_values(arr)
+    arr.inject(self) {|o, a| o.send(:where, property_type_where_value(a)) }
+  end
+
+  def self.property_type_where_value(arg)
+    case arg
+    when "flats"
+      "property_type='F'"
+    when "terraced"
+      "property_type='T'"
+    when "semi_detached"
+      "property_type='S'"
+    when "detached"
+      "property_type='D'"
+    when "other"
+      "property_type='O'"
+    end
+  end
 end
